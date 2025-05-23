1b8ab754df297ab49b0e21deb237ae54bd4a6707
terapeutaLPG <terapeutaLPG@gmail.com>
2025-05-23 21:10:12 +0200

    dzialaplugin

--- src/main/java/me/twojplugin/commands/LastLoginCommand.java
+++ src/main/java/me/twojplugin/commands/LastLoginCommand.java
@@ -2,6 +2,7 @@ package me.twojplugin.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -23,41 +24,77 @@ public class LastLoginCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender snd, Command cmd, String lbl, String[] args) {
-        String t = args.length == 1 ? args[0] : snd.getName();
-        if (!auth.isRegistered(t)) {
-            snd.sendMessage(ChatColor.RED + "Gracz " + t + " nie jest zarejestrowany.");
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        String target = args.length == 1 ? args[0] : sender.getName();
+        if (!auth.isRegistered(target)) {
+            sender.sendMessage(ChatColor.RED + "Gracz " + target + " nie jest zarejestrowany.");
             return true;
         }
-        boolean on = Bukkit.getPlayerExact(t) != null;
-        snd.sendMessage(ChatColor.GOLD + "=== /lastlogin " + t + " ===");
-        snd.sendMessage(ChatColor.YELLOW + "Zarejestrowany:     " + auth.getRegisteredDate(t));
-        snd.sendMessage(ChatColor.YELLOW + "Ost. logowanie:     " + auth.getLastLoginDate(t));
-        snd.sendMessage(ChatColor.YELLOW + "Ost. wejście:       " + auth.getLastJoinDate(t));
-        snd.sendMessage(ChatColor.YELLOW + "Ost. wyjście:       " + auth.getLastQuitDate(t));
-        snd.sendMessage(ChatColor.YELLOW + "Online: " + (on ? "TAK" : "NIE"));
 
-        String loc = auth.getLastLogoutLocation(t);
-        if (snd instanceof Player && !loc.equals("–")) {
-            String[] p = loc.split(" ");
-            if (p.length == 4) {
-                String w = p[0], x = p[1], y = p[2], z = p[3];
-                TextComponent tc = new TextComponent(
-                        ChatColor.YELLOW + "Teleport do: " + ChatColor.AQUA + loc
-                );
-                tc.setClickEvent(new ClickEvent(
-                        Action.RUN_COMMAND,
-                        "/execute in " + w + " run tp @s " + x + " " + y + " " + z
-                ));
-                tc.setHoverEvent(new HoverEvent(
-                        HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Kliknij by teleportować").create()
-                ));
-                ((Player) snd).spigot().sendMessage(tc);
-                return true;
-            }
+        boolean online = Bukkit.getPlayerExact(target) != null;
+        sender.sendMessage(ChatColor.GOLD + "=== /lastlogin " + target + " ===");
+        sender.sendMessage(ChatColor.YELLOW + "Zarejestrowany:     " + auth.getRegisteredDate(target));
+        sender.sendMessage(ChatColor.BLUE + "Ostatnie logowanie: " + auth.getLastLoginDate(target));
+        sender.sendMessage(ChatColor.YELLOW + "Ostatnie wejście:   " + auth.getLastJoinDate(target));
+        sender.sendMessage(ChatColor.RED + "Ostatnie wyjście:   " + auth.getLastQuitDate(target));
+        sender.sendMessage(ChatColor.GREEN + "Online: " + (online ? "TAK" : "NIE"));
+
+        String loc = auth.getLastLogoutLocation(target);
+        if ("–".equals(loc)) {
+            sender.sendMessage(ChatColor.YELLOW + "Miejsce wylogowania: " + ChatColor.AQUA + loc);
+            return true;
         }
-        snd.sendMessage(ChatColor.YELLOW + "Miejsce wylogowania: " + ChatColor.AQUA + loc);
+
+        String[] parts = loc.split(" ");
+        if (parts.length != 4) {
+            sender.sendMessage(ChatColor.RED + "Miejsce wylogowania: " + ChatColor.AQUA + loc);
+            return true;
+        }
+
+        String worldName = parts[0];
+        String x = parts[1], y = parts[2], z = parts[3];
+
+        // mapowanie środowisk na dimension IDs
+        String dimID;
+        World wObj = Bukkit.getWorld(worldName);
+        if (wObj != null) {
+            switch (wObj.getEnvironment()) {
+                case NORMAL ->
+                    dimID = "minecraft:overworld";
+                case NETHER ->
+                    dimID = "minecraft:the_nether";
+                case THE_END ->
+                    dimID = "minecraft:the_end";
+                default ->
+                    dimID = worldName;
+            }
+        } else {
+            dimID = worldName;
+        }
+
+        if (sender instanceof Player p) {
+            // zamiast @s – jawnie nick, żeby Essentials go poprawnie parsowało
+            String cmdTp = "/execute in " + dimID
+                    + " run tp " + p.getName()
+                    + " " + x + " " + y + " " + z;
+
+            TextComponent tc = new TextComponent(
+                    ChatColor.YELLOW + "Teleport do: "
+                    + ChatColor.AQUA + x + " " + y + " " + z
+                    + ChatColor.GRAY + " [" + worldName + "]"
+            );
+            tc.setClickEvent(new ClickEvent(Action.RUN_COMMAND, cmdTp));
+            tc.setHoverEvent(new HoverEvent(
+                    HoverEvent.Action.SHOW_TEXT,
+                    new ComponentBuilder("Kliknij, aby teleportować do ostatniego miejsca wylogowania").create()
+            ));
+            p.spigot().sendMessage(tc);
+        } else {
+            sender.sendMessage(ChatColor.YELLOW + "Miejsce wylogowania: "
+                    + ChatColor.AQUA + x + " " + y + " " + z
+                    + ChatColor.GRAY + " [" + worldName + "]");
+        }
+
         return true;
     }
 }
